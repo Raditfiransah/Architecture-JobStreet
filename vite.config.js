587fe88bff0a5b3bd89d48ec17669863624c6266
raditfiransah <raditfiransah180@gmail.com>
2026-04-02 20:40:50 +0700

    feat: initialize new Laravel project structure and core application files

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import inertia from '@inertiajs/vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
+            refresh: true,
+        }),
+        inertia(),
+        tailwindcss(),
+    ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+})

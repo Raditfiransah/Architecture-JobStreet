4da8a8ea760fe1adfd84d20eb52c1422ee1ebb94
raditfiransah <raditfiransah180@gmail.com>
2026-04-04 12:18:07 +0700

    auth done

--- app/Models/EmailVerificationCode.php
+++ app/Models/EmailVerificationCode.php
@@ -7,10 +7,22 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-#[Fillable(['user_id', 'code', 'is_used', 'used_at', 'expired_at'])]
 class EmailVerificationCode extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'code',
+        'is_used',
+        'used_at',
+        'expired_at',
+    ];
 
     protected function casts(): array
     {
--- app/Models/User.php
+++ app/Models/User.php
@@ -13,12 +13,36 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Mail;
 
-#[Fillable(['name', 'email', 'password', 'role', 'is_active', 'is_verified', 'avatar_url', 'email_verified_at'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+        'is_active',
+        'is_verified',
+        'avatar_url',
+        'email_verified_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
     protected function casts(): array
     {
